520214d2649529643316e517daaf155bd52c5f16
Maxi <maxi21498@gmail.com>
2021-07-30 18:34:29 -0300

    Biblioteca (Final video 45)

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import crearAutor,editarAutor,eliminarAutor,ListadoAutores,ActualizarAutor
+from .views import crearAutor,editarAutor,listarAutor,eliminarAutor,ListadoAutores,ActualizarAutor,CrearAutor,EliminarAutor
 
 urlpatterns = [
-    path('crear_autor/',crearAutor, name = 'crear_autor'),
+    path('crear_autor/',CrearAutor.as_view(), name = 'crear_autor'),
     path('listar_autor/',ListadoAutores.as_view(), name = 'listar_autor'),
     path('editar_autor/<int:pk>',ActualizarAutor.as_view(), name = 'editar_autor'),
-    path('eliminar_autor/<int:id>',eliminarAutor, name = 'eliminar_autor')
+    path('eliminar_autor/<int:pk>',EliminarAutor.as_view(), name = 'eliminar_autor')
 ]
--- apps/libro/views.py
+++ apps/libro/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from django.core.exceptions import ObjectDoesNotExist
+from django.views.generic.edit import DeleteView
 from .forms import AutorForm
 from .models import Autor
-from django.views.generic import TemplateView,ListView,UpdateView
+from django.views.generic import TemplateView,ListView,UpdateView,CreateView,DetailView
 from django.urls import reverse_lazy
 # Create your views here.
 
@@ -26,10 +27,50 @@ class ListadoAutores(ListView):
 
 
 class ActualizarAutor(UpdateView):
-    model=Autor
     template_name="libro/crear_autor.html"
+    model=Autor
     form_class = AutorForm
     success_url = reverse_lazy("libro:listar_autor")
+
+
+class CrearAutor(CreateView):
+    template_name='libro/crear_autor.html'
+    model=Autor
+    form_class = AutorForm
+    success_url = reverse_lazy("libro:listar_autor")
+
+
+
+class EliminarAutor(DeleteView):
+    model = Autor
+    success_url = reverse_lazy("libro:listar_autor")
+
+    def post(self, request,pk,*args,**kwargs):
+        object=Autor.objects.get(id = pk)
+        object.estado=False
+        object.save()
+
+        return redirect('libro:listar_autor')
+
+
+
+def eliminarAutor(request,id):
+    autor = Autor.objects.get(id = id)
+    autor.estado = False
+    autor.save()
+    return redirect('libro:listar_autor')
+
+
+def crearAutor(request):
+    if request.method == 'POST':
+        autor_form = AutorForm(request.POST)
+        if autor_form.is_valid():
+            autor_form.save()
+            return redirect('index')
+    else:
+        autor_form = AutorForm()
+    return render(request,'libro/crear_autor.html',{'autor_form':autor_form})
+
 
 
 
@@ -49,29 +90,15 @@ def editarAutor(request,id):
         error = e
     return render(request,'libro/crear_autor.html',{'autor_form':autor_form,'error':error})
 
-def crearAutor(request):
-    if request.method == 'POST':
-        autor_form = AutorForm(request.POST)
-        if autor_form.is_valid():
-            autor_form.save()
-            return redirect('index')
-    else:
-        autor_form = AutorForm()
-    return render(request,'libro/crear_autor.html',{'autor_form':autor_form})
 
 
 
 
 
-def eliminarAutor(request,id):
-    autor = Autor.objects.get(id = id)
-    autor.estado = False
-    autor.save()
-    return redirect('libro:listar_autor')
 
 
-"""
-    def listarAutor(request):
-        autores = Autor.objects.filter(estado = True)
-        return render(request,'libro/listar_autor.html',{'autores':autores})
-"""
+
+def listarAutor(request):
+    autores = Autor.objects.filter(estado = True)
+    return render(request,'libro/listar_autor.html',{'autores':autores})
+
